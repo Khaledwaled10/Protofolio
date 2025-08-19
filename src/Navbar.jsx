6a0eacdbddf3b017e5e370efe22cbc044cc6461d
Khaledwaled10 <khaledwaled8713@gmail.com>
2025-08-19 17:33:41 +0300

    Fix token fetch inside API functions to avoid stale token issue

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,36 +1,45 @@
-import React from 'react'
-import { Link, Links, NavLink } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-    <nav className="bg-dark navbar navbar-expand-lg p-3">
-  <div className="container-fluid ms-5">
-    <Link className="navbar-brand clr font-xl ms-5" to="/">PROTFOLIO</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-lg-5  mb-2 mb-lg-0 ms-auto">
+      <nav className="bg-dark navbar navbar-expand-lg p-3">
+        <div className="container-fluid ms-lg-5">
+          <Link className="navbar-brand clr font-xl ms-lg-5" to="/">PROTFOLIO</Link>
+          
+          {/* زرار الموبايل */}
+          <button 
+            className="navbar-toggler text-white border-0" 
+            type="button" 
+            onClick={toggleMenu}
+          >
+            {/* 3 شرط (Hamburger icon) */}
+            <span style={{fontSize: "24px"}}>☰</span>
+          </button>
 
-        <li className="nav-item ms-2">
-          <NavLink className="nav-link" to='/about' >About</NavLink>
-        </li>
-                <li className="nav-item ms-2">
-          <NavLink className="nav-link" to='/protfolio' >Protfolio</NavLink>
-        </li>
-
-                <li className="nav-item ms-2 me-4 me-lg-5">
-          <NavLink className="nav-link me-lg-5" to='/contact' >Contact</NavLink>
-        </li>
-        
-      </ul>
-      
-      
-    </div>
-  </div>
-</nav>
-
+          {/* القائمة */}
+          <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`} id="navbarSupportedContent">
+            <ul className="navbar-nav me-lg-5 mb-2 mb-lg-0 ms-auto">
+              <li className="nav-item ms-2">
+                <NavLink className="nav-link" to='/about'>About</NavLink>
+              </li>
+              <li className="nav-item ms-2">
+                <NavLink className="nav-link" to='/protfolio'>Protfolio</NavLink>
+              </li>
+              <li className="nav-item ms-2 me-4 me-lg-5">
+                <NavLink className="nav-link me-lg-5" to='/contact'>Contact</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
